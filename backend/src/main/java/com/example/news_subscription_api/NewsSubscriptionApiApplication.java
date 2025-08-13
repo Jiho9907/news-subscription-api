455c00c17129e0408a102f11f8cec03f67a872a8
Choijiho <cjhwt@naver.com>
2025-08-13 15:26:22 +0900

    폴더 구조 리팩토링

---
+++ backend/src/main/java/com/example/news_subscription_api/NewsSubscriptionApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.news_subscription_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsSubscriptionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsSubscriptionApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/news_subscription_api/NewsSubscriptionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.news_subscription_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsSubscriptionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
