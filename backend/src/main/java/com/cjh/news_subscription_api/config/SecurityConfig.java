ac8f5fb39585760ce20ec45bf6f942662cf4308e
Choijiho <cjhwt@naver.com>
2025-08-13 23:49:28 +0900

    feat : passwordEncoder Bean 등록

--- backend/src/main/java/com/cjh/news_subscription_api/config/SecurityConfig.java
+++ backend/src/main/java/com/cjh/news_subscription_api/config/SecurityConfig.java
@@ -1,4 +1,21 @@
+/**
+ * JWT 기반 인증을 사용하는 스프링 시큐리티 설정
+ * */
 package com.cjh.news_subscription_api.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+//@RequiredArgsConstructor
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
